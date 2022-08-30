2002c94216c7d9fb74315924151d1814b92a179c
Wu Rui <wuruiwm@qq.com>
2022-08-30 09:02:28 +0800

    gin 异常路由处理

--- server/initialize/server.go
+++ server/initialize/server.go
@@ -2,6 +2,7 @@ package initialize
 
 import (
 	"app/api/middleware"
+	"app/api/response"
 	"app/global"
 	"app/router"
 	"fmt"
@@ -24,8 +25,15 @@ func Server() {
 	r.Use(middleware.Recover)
 	//上传文件目录
 	r.Static("/upload", "./resource/upload")
+	//处理不存在的路由
+	r.NoMethod(ErrRoute)
+	r.NoRoute(ErrRoute)
 	//路由设置
 	r = router.Router(r)
 	//启动服务
 	_ = r.Run(fmt.Sprintf("0.0.0.0:%d", global.Config.ServerPort))
+}
+
+func ErrRoute(c *gin.Context) {
+	response.Error(c, fmt.Sprintf("api不存在 path:%s method:%s", c.Request.URL, c.Request.Method))
 }
