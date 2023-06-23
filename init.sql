bdc2802406696ee0ca85a16b94e2e1c261ba9428
吴瑞 <wuruiwm@qq.com>
2023-06-23 18:54:28 +0800

    去除init.sql 注释头信息

--- init.sql
+++ init.sql
@@ -1,19 +1,3 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : mysql
- Source Server Type    : MySQL
- Source Server Version : 80016
- Source Host           : mysql.nikm.cn:3306
- Source Schema         : my_new
-
- Target Server Type    : MySQL
- Target Server Version : 80016
- File Encoding         : 65001
-
- Date: 23/08/2022 09:05:52
-*/
-
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
