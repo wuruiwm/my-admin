6f59ca87133272f040b82e6034a9bc623431b823
wurui <wuruiwm@qq.com>
2023-10-17 19:14:21 +0800

    m3u8 url长度改为1000

--- server/model/m3u8.go
+++ server/model/m3u8.go
@@ -4,7 +4,7 @@ package model
 type M3u8 struct {
 	Base
 	Name    string `gorm:"column:name;type:char(50);comment:音乐名;NOT NULL" json:"name"`
-	Url     string `gorm:"column:url;type:varchar(100);comment:m3u8视频地址;NOT NULL" json:"url"`
+	Url     string `gorm:"column:url;type:varchar(1000);comment:m3u8视频地址;NOT NULL" json:"url"`
 	Status  uint   `gorm:"column:status;type:tinyint(1) unsigned;default:0;comment:状态 0待执行 1执行成功 2执行失败;NOT NULL" json:"status"`
 	Command string `gorm:"column:command;type:char(200);comment:执行的命令;NOT NULL" json:"command"`
 	Content string `gorm:"column:content;type:text;comment:命令执行日志;NOT NULL" json:"content"`
