8c54676aa19640e916750c8e4a08fd20085dbe69
wurui <wuruiwm@qq.com>
2023-10-21 15:11:48 +0800

    字段类型修改为longtext

--- server/model/youtube.go
+++ server/model/youtube.go
@@ -7,7 +7,7 @@ type Youtube struct {
 	Url     string `gorm:"column:url;type:varchar(100);comment:youtube视频地址;NOT NULL" json:"url"`
 	Status  uint   `gorm:"column:status;type:tinyint(1) unsigned;default:0;comment:状态 0待执行 1执行成功 2执行失败;NOT NULL" json:"status"`
 	Command string `gorm:"column:command;type:char(200);comment:执行的命令;NOT NULL" json:"command"`
-	Content string `gorm:"column:content;type:text;comment:命令执行日志;NOT NULL" json:"content"`
+	Content string `gorm:"column:content;type:longtext;comment:命令执行日志;NOT NULL" json:"content"`
 }
 
 func (m *Youtube) TableName() string {
