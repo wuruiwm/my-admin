a7e4c9a7aa3fcaf29d10eb8b7ff99c6cdf391e95
wurui <wuruiwm@qq.com>
2023-12-28 20:02:51 +0800

    ssl证书兼容多域名

--- server/api/controller/admin/ssl.go
+++ server/api/controller/admin/ssl.go
@@ -1,20 +1,13 @@
 package admin
 
 import (
-	"app/api/request"
 	"app/api/response"
 	"app/logic"
-	"app/util"
 	"github.com/gin-gonic/gin"
 )
 
 func Ssl(c *gin.Context) {
-	param := &request.Ssl{}
-	if err := c.ShouldBindQuery(param); err != nil {
-		response.Error(c, util.ValidatorError(err))
-		return
-	}
-	if result, err := logic.Ssl(param); err == nil {
+	if result, err := logic.SslList(); err == nil {
 		response.Success(c, "success", result)
 	} else {
 		response.Error(c, err.Error())
--- server/api/response/ssl.go
+++ server/api/response/ssl.go
@@ -3,6 +3,7 @@ package response
 type Ssl struct {
 	Key        string   `json:"key"`
 	Pem        string   `json:"pem"`
-	Domain     []string `json:"domain"`
+	Domain     string   `json:"domain"`
+	List       []string `json:"list"`
 	ExpireTime string   `json:"expire_time"`
 }
